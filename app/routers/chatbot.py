6aba116c48b54c3a9f9d681810395035c72460d7
Judd Ebert <juddebert@yahoo.com>
2025-07-12 14:28:12 -0400

    gemini

--- app/routers/chatbot.py
+++ app/routers/chatbot.py
@@ -1,7 +1,15 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
 
 router = APIRouter(prefix="/chatbot", tags=["Chatbot"])
+
+client = genai.Client(api_key=api_key)
 
 class ChatbotRequest(BaseModel):
     message: str
@@ -11,5 +19,11 @@ class ChatbotResponse(BaseModel):
 
 @router.post("/ask", response_model=ChatbotResponse)
 def ask_chatbot(request: ChatbotRequest):
-    # For now, return a dummy response
-    return ChatbotResponse(response="This is a dummy chatbot response.")
+    response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents= f"You are a financial expert, teaching teens financial literacy. You will use easy to understand language, analogies, and encourage the student in their learning. Their query is: {request.message}. Please respond in the most appropriate manner to help them learn."
+    )
+    print(response.text)
+    if(response.text != ""):
+        return ChatbotResponse(response= "dummy testing")
+    
